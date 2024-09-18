80cbb066707066a86eaf2cdb8d910212f1f65186
ShevelevMark <shevelev.mm@yandex.ru>
2024-09-18 14:04:37 +0300

    Основной цикл отрисовки

--- src/main.c
+++ src/main.c
@@ -1,6 +1,20 @@
 #include <stdio.h>
+#include <conio.h>
+#include <unistd.h>
+
 
 int main() {
-    printf("Project Snake!");
+    unsigned frame_cnt = 0;
+    while (1) {
+        printf("Frame %u\n", ++frame_cnt);
+        if (kbhit()) {
+            int key = getch();
+            printf("key code = %d\n", key);
+            if (27 == key)
+                break;
+        }
+        sleep(1);
+        system("cls");
+    }
     return 0;
 }
